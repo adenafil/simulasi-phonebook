2e06eae23ef74e84b005c0361c7b23e01e3615c1
adenafil <nafilie9@gmail.com>
2023-10-29 06:20:15 +0700

    Membuat repository dan folder untuk pengerjaan praktikum 1 ASD

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace praktikumASD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
