e19cec669b63e9a4582ce277740e249a6f163cac
adenafil <nafilie9@gmail.com>
2023-10-29 12:23:58 +0700

    feat: Add Delete Method to DoublyLinkedList Class in util folder

--- util/DoublyLinkedList.cs
+++ util/DoublyLinkedList.cs
@@ -28,6 +28,33 @@ namespace util
 
         }
 
+        public void Delete(T data)
+        {
+            Node<T> sekarang = head;
+            
+            while(sekarang != null)
+            {
+                if (sekarang.data.Equals(data))
+                {
+                    if (sekarang.prev != null)
+                    {
+                        sekarang.prev.next = sekarang.next;
+                    }
+                    else
+                    {
+                        head = sekarang.next;
+                    }
+                    if (sekarang.next != null)
+                    {
+                        sekarang.next.prev = sekarang.prev;
+                    }
+                    return;
+                }
+                sekarang = sekarang.next;
+            }
+
+        }
+
 
         public void PrintList()
         {
