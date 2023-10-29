0b7ca30010f0f0782619ef85dbb75267f0c7b906
adenafil <nafilie9@gmail.com>
2023-10-29 07:08:41 +0700

    feat: menambahkan Class Contact

---
+++ util/Contact.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace util
+{
+    public class Contact
+    {
+        private int noTelepon;
+        private String nama;
+        private String email;
+
+        public Contact(int noTelepon, String nama, String email) {
+            this.noTelepon = noTelepon;
+            this.nama = nama;
+            this.email = email;
+        }
+    }
+}
